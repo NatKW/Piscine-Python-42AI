89cc73820d0bcc38c8b70036c704fb43cb3ae4d4
NatKW <92329992+NatKW@users.noreply.github.com>
2022-11-17 19:47:25 +0100

    ex02

--- Module01/ex02/test.py
+++ Module01/ex02/test.py
@@ -1,12 +1,31 @@
-import vector
 from vector import Vector
 
-e1 = Vector("Error Test")
-v1 = Vector(5)
-v2 = Vector((10, 16))
-v3 = Vector(range(2, 7))
-v4 = Vector([1.3, 4.8, 9.1])
-v5 = Vector([1, 4, 9])
-v = [v1, v2, v3, v4, v5]
-for i in v:
-    print(i.values)
+if __name__ == "__main__": 
+
+    e1 = Vector("Error Test")
+
+    print("Error test: ", e1)
+
+    v1 = Vector(5)
+    v2 = Vector((10, 16))
+    
+    v4 = Vector([1.3, 4.8, 9.1])
+    v5 = Vector([1, 4, 9])
+
+    v6 = Vector([[0.0], [1.0], [2.0], [3.0]])
+
+    v = [v1, v2, v4, v5, v6]
+
+    for i in v:
+        print(i.values)
+        print(i.size)
+        print(i.shape)
+
+# Row vector of shape (1, n)
+# = list of floats
+#Vector([[0.0, 1.0, 2.0, 3.0]]).shape)
+# Expected output : (1,4)  
+# Column vector of shape (n, 1)
+# = list of lists of single floats
+#Vector([[0.0], [1.0], [2.0], [3.0]]).shape)
+# Expected output:(4,1)
--- Module01/ex02/vector.py
+++ Module01/ex02/vector.py
@@ -1,11 +1,16 @@
 class Vector:
-    @staticmethod
-    def check_shape_col(col: list):
-        return all(isinstance(num, float) for num in col)
-
-    @staticmethod
-    def check_shape_row(row: list):
-        
-
-
-    def __init__(self, values):
+    def __init__(self, object):
+        if type(object) is int:
+            self.values = [float(d) for d in range(int(object))]
+            self.size = object
+            self.shape = (1, self.size)
+        elif type(object)is list:
+            self.values = object
+            self.size = len(object)
+            self.shape = (self.size, 1)
+        elif type(object) is tuple and len(object) == 2:
+            self.values =[float(o) for o in range(object[0], object[1])] 
+            self.size = len(self.values)
+            self.shape = (1, self.size)
+        else:
+            print("Wrong Init of class Vector: \nobject must be list[float] / list[list[float]] / int / tuple[int] ...")       
