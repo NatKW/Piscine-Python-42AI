672380f4282ca42d117da85e7569198534a31f4c
Nathalie DE LA BURGADE <nade-la-@e1r10p11.clusters.42paris.fr>
2022-11-25 19:33:22 +0100

    en cours

---
+++ Module01/ex03/generator.py
@@ -0,0 +1,39 @@
+def generator(text, sep=" ", option=None):
+	""" 
+	Splits the text according to sep value and yield the substrings.
+	option precise if an action is performed to the substrings before it is yielded.
+
+	shuffle : shuffles the list f words
+	unique : returns a list where each word appears only once
+	ordered: alphabetically sorts the words
+	"""
+	try:
+		words = text.slit(sep)
+		for word in words:
+			yield word
+	except:
+		if type(text) is not str \
+		or option not in ['None', 'shuffle', 'unique', 'ordered']:
+			yield "Error"
+
+	
+
+
+
+
+if __name__ == '__main__':
+    text = "Le Lorem Ipsum est simplememt du faux texte."
+	for word in generator(generator)
+		if word
+			print(word)
+	print("\n")
+	# for word in generator(text, sep=" ", option="shuffle"):
+	# 	print(word)
+	# for word in generator(text, sep=" ", option="ordered"):
+	# 	print(word)
+	# text = "Lorem Ipsum Lorem Ipsum"
+	# for word in generator(text, sep=" ", option="unique"):
+	# 	print(word)
+	# text = 1.0
+	# for word in generator(text, sep="."):
+	# 	print(word)
